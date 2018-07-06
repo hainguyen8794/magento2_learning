a2c5cfa14eeafbb45621407675aa1ff7ad31404f
hainh <hainh080794@gmail.com>
2018-07-06 17:27:43 +0700

    done mass action

---
+++ app/code/OpenTechiz/Blog/Controller/Adminhtml/Post/NewAction.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hainh
+ * Date: 06/07/2018
+ * Time: 16:57
+ */
+
+namespace OpenTechiz\Blog\Controller\Adminhtml\Post;
+
+
+class NewAction extends \Magento\Backend\App\Action
+{
+    protected $resultForwardFactory;
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Backend\Model\View\Result\ForwardFactory $resultForwardFactory
+    ) {
+        $this->resultForwardFactory = $resultForwardFactory;
+        parent::__construct($context);
+    }
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('OpenTechiz_Blog::save');
+    }
+    /**
+     * Forward to edit
+     *
+     * @return \Magento\Backend\Model\View\Result\Forward
+     */
+    public function execute()
+    {
+        /** @var \Magento\Backend\Model\View\Result\Forward $resultForward */
+        $resultForward = $this->resultForwardFactory->create();
+        return $resultForward->forward('edit');
+    }
+}
