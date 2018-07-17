55eb4999193d973a96419c2b64d85afb71c2d5cf
hainh <hainh080794@gmail.com>
2018-07-17 14:32:41 +0700

    add customer_product

---
+++ app/code/Robin/Bai1/Controller/Adminhtml/Index/Index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hainh
+ * Date: 03/07/2018
+ * Time: 23:26
+ */
+
+namespace Robin\Bai1\Controller\Adminhtml\Index;
+
+use Magento\Framework\App\Action\Context;
+use \Robin\Bai1\Model\BannerFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $_resultPageFactory;
+    protected $_registry;
+    protected $bannerFactory;
+    public function __construct(Context $context,
+                                \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+                                \Magento\Framework\Registry $registry
+    , BannerFactory $bannerFactory)
+    {
+        $this->bannerFactory =$bannerFactory;
+        $this->_resultPageFactory = $resultPageFactory;
+        $this->_registry = $registry;
+        parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        $resultPage->getConfig()->getTitle()->prepend(__('Banner'));
+        return $resultPage;
+    }
+}
